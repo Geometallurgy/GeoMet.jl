80eb1ac1998f0493fc8fec2b4823a6d560a0961d
GeoMet.jl <geomet.jl@gmail.com>
2025-06-18 16:24:09 -0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using GeoMet
 using Test
+using DataFrames
 
 @testset "GeoMet.jl" begin
     # Test with known values
@@ -8,5 +9,9 @@ using Test
     # Error tests
     @test_throws ErrorException calculate_bwi(0, 200, 1.0, 1.0)  # F80 inválido
     @test_throws ErrorException calculate_bwi(2000, 2000, 1.0, 1.0)  # Denominador zero
+
+    # DataFrame test
+    df = DataFrame(F80=[2000, 1500], P80=[200, 150], M=[1.0, 1.1], A=[1.0, 1.05])
+    @test length(calculate_bwi(df)) == 2
     
 end
