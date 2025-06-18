dad479647a503da963405685c2838a765451985a
GeoMet.jl <geomet.jl@gmail.com>
2025-06-18 16:07:32 -0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@ using GeoMet
 using Test
 
 @testset "GeoMet.jl" begin
-    # Write your tests here.
+    # Test with known values
+    @test isapprox(calculate_bwi(2174, 97, 0.81, 150), 23.023, atol=0.001)  # atol is the absolute tolerance
+    
+    # Error tests
+    @test_throws ErrorException calculate_bwi(0, 200, 1.0, 1.0)  # F80 inválido
+    @test_throws ErrorException calculate_bwi(2000, 2000, 1.0, 1.0)  # Denominador zero
+    
 end
