0d5ea224e7c67ce417b915760c68bb748ac36e2c
Geometallurgy <geomet.jl@gmail.com>
2025-07-06 13:32:45 -0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -35,6 +35,6 @@ end
     # DataFrame test
     df = DataFrame(F80=[2.0, 1.5], P80=[0.2, 0.15]) 
     results = calculate_specific_energy_charles(df; K=1000.0, n=1.0)
-    @test isapprox(results[1], 9500.0, atol=0.01)
+    @test isapprox(results[1], 4500.0, atol=0.01)
 
 end
