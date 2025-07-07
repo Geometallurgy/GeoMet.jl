3dde99b84a49cf9b5f5fb509bf371bba22d487be
Geometallurgy <geomet.jl@gmail.com>
2025-07-07 20:53:41 -0300

    Update GeoMet.jl

--- src/GeoMet.jl
+++ src/GeoMet.jl
@@ -42,9 +42,9 @@ function calculate_specific_energy_morrell(F80::Real, P80::Real, Mi::Real)
         throw(ArgumentError("F80 and P80 must be different."))
     end
 
-    f(x) = (0.295 + x / 1_000_000) / x
-
-    return Mi * 4 * (f(P80) - f(F80)) * 1000
+    f(x) = (0.295 + x / 1_000_000)  
+    
+    return Mi * 4 * (f(P80)/P80 - f(F80)/F80)  
 end
 # dataframes version
 function calculate_specific_energy_morrell(df::AbstractDataFrame; F80=:F80, P80=:P80, Mi::Symbol=:Mi)
